f0f9c1c608b46cfe8f77b2fe4ce64f0835ea4b35
obaeza16 <baeza@recerca.clinic.cat>
2024-11-06 16:54:06 +0100

    Finished compositional with tascCODA

--- sc_compositional_tascoda.py
+++ sc_compositional_tascoda.py
@@ -149,3 +149,216 @@ sc.pp.neighbors(adata, use_rep="X_scVI")
 sc.tl.umap(adata)
 
 sc.pl.umap(adata, color=["condition", "batch", "cell_label"], ncols=3, wspace=0.25)
+
+# If no neighbors_key parameter is specified, Milo uses the neighbours from .obsp.
+# Therefore, ensure that sc.pp.neighbors was run on the correct representation,
+# i.e. an integrated latent space if batch correction was required.
+
+milo.make_nhoods(mdata, prop=0.1)
+# Now the binary assignment of cells to neighbourhood is stored in adata.obsm['nhoods']
+adata.obsm["nhoods"]
+# We check the mean number of cells in each neigh, to make sure we can detect 
+# differences between samples
+nhood_size = adata.obsm["nhoods"].toarray().sum(0)
+plt.hist(nhood_size, bins=20)
+plt.xlabel("# cells in neighbourhood")
+plt.ylabel("# neighbouthoods")
+
+np.median(nhood_size)
+# Based on the plot above, we have a large number of neighbourhoods with less than 
+# 30 cells, which could lead to an underpowered test. To solve this, we just need
+# to recompute the KNN graph using n_neighbors=30. To distinguish this KNN graph
+# used for neighbourhood-level DA analysis from the graph used for UMAP building,
+# we will store this as a distinct graph in adata.obsp.
+
+sc.pp.neighbors(adata, n_neighbors=30, use_rep="X_scVI", key_added="milo")
+milo.make_nhoods(mdata, neighbors_key="milo", prop=0.1)
+# Lets check that the distribution has shifted:
+nhood_size = adata.obsm["nhoods"].toarray().sum(0)
+plt.hist(nhood_size, bins=20)
+plt.xlabel("# cells in neighbourhood")
+plt.ylabel("# neighbouthoods")
+
+# In the next step, Milo counts cells belonging to each of the samples (here
+# identified by the batch column in adata.obs).
+
+milo.count_nhoods(mdata, sample_col="batch")
+mdata["milo"]
+# We can verify that the number of cells per sample times the number of samples
+# roughly corresponds to the number of cells in a neighbourhood.
+mean_n_cells = mdata["milo"].X.toarray().mean(0)
+plt.plot(nhood_size, mean_n_cells, ".")
+plt.xlabel("# cells in nhood")
+plt.ylabel("Mean # cells per sample in nhood")
+
+# Milo uses edgeR’s QLF test to test if there are statistically significant 
+# differences between the number of cells from a condition of interest in 
+# each neighborhood.
+
+# Let’s first test for differences associated with Salmonella infection.
+
+milo.da_nhoods(
+    mdata, design="~condition", model_contrasts="conditionSalmonella-conditionControl"
+)
+milo_results_salmonella = mdata["milo"].obs.copy()
+milo_results_salmonella
+
+# For each neighbourhood, we calculate a set of statistics.
+# Before any exploration and interpretation of the results, we can visualize these
+# statistics with a set of diagnostics plots to sanity check our statistical test
+def plot_milo_diagnostics(mdata):
+    alpha = 0.1  ## significance threshold
+
+    with matplotlib.rc_context({"figure.figsize": [12, 12]}):
+
+        ## Check P-value histogram
+        plt.subplot(2, 2, 1)
+        plt.hist(mdata["milo"].var["PValue"], bins=20)
+        plt.xlabel("Uncorrected P-value")
+
+        ## Visualize extent of multiple-testing correction
+        plt.subplot(2, 2, 2)
+        plt.scatter(
+            mdata["milo"].var["PValue"],
+            mdata["milo"].var["SpatialFDR"],
+            s=3,
+        )
+        plt.xlabel("Uncorrected P-value")
+        plt.ylabel("SpatialFDR")
+
+        ## Visualize volcano plot
+        plt.subplot(2, 2, 3)
+        plt.scatter(
+            mdata["milo"].var["logFC"],
+            -np.log10(mdata["milo"].var["SpatialFDR"]),
+            s=3,
+        )
+        plt.axhline(
+            y=-np.log10(alpha),
+            color="red",
+            linewidth=1,
+            label=f"{int(alpha*100)} % SpatialFDR",
+        )
+        plt.legend()
+        plt.xlabel("log-Fold Change")
+        plt.ylabel("- log10(SpatialFDR)")
+        plt.tight_layout()
+
+        ## Visualize MA plot
+        df = mdata["milo"].var
+        emp_null = df[df["SpatialFDR"] >= alpha]["logFC"].mean()
+        df["Sig"] = df["SpatialFDR"] < alpha
+
+        plt.subplot(2, 2, 4)
+        sns.scatterplot(data=df, x="logCPM", y="logFC", hue="Sig")
+        plt.axhline(y=0, color="grey", linewidth=1)
+        plt.axhline(y=emp_null, color="purple", linewidth=1)
+        plt.legend(title=f"< {int(alpha*100)} % SpatialFDR")
+        plt.xlabel("Mean log-counts")
+        plt.ylabel("log-Fold Change")
+        plt.show()
+
+
+plot_milo_diagnostics(mdata)
+
+# After sanity check, we can visualize the DA results for each neighbourhood
+# by the position of the index cell on the UMAP embedding, to qualitatively 
+# assess which cell types may be most affected by the infection.
+
+milo.annotate_nhoods(mdata, anno_col="cell_label")
+# Define as mixed if fraction of cells in nhood with same label is lower than 0.75
+
+mdata["milo"].var.loc[
+    mdata["milo"].var["nhood_annotation_frac"] < 0.75, "nhood_annotation"
+] = "Mixed"
+
+milo.plot_da_beeswarm(mdata)
+plt.show()
+
+## Turn into continuous variable
+mdata["rna"].obs["Hpoly_timecourse"] = (
+    mdata["rna"]
+    .obs["condition"]
+    .cat.reorder_categories(["Salmonella", "Control", "Hpoly.Day3", "Hpoly.Day10"])
+)
+mdata["rna"].obs["Hpoly_timecourse"] = mdata["rna"].obs["Hpoly_timecourse"].cat.codes
+
+## Here we exclude salmonella samples
+test_samples = (
+    mdata["rna"]
+    .obs.batch[mdata["rna"].obs.condition != "Salmonella"]
+    .astype("str")
+    .unique()
+)
+milo.da_nhoods(mdata, design="~ Hpoly_timecourse", subset_samples=test_samples)
+
+plot_milo_diagnostics(mdata)
+with matplotlib.rc_context({"figure.figsize": [10, 10]}):
+    milo.plot_nhood_graph(mdata, alpha=0.1, min_size=5, plot_edges=False)
+    
+milo.plot_da_beeswarm(mdata)
+plt.show()
+
+# We can verify that the test captures a linear increase in cell numbers across 
+# the time course by plotting the number of cells per sample by condition in 
+# neighborhoods where significant enrichment or depletion was detected.
+
+entero_ixs = mdata["milo"].var_names[
+    (mdata["milo"].var["SpatialFDR"] < 0.1)
+    & (mdata["milo"].var["logFC"] < 0)
+    & (mdata["milo"].var["nhood_annotation"] == "Enterocyte")
+]
+
+plt.title("Enterocyte")
+milo.plot_nhood_counts_by_cond(
+    mdata, test_var="Hpoly_timecourse", subset_nhoods=entero_ixs
+)
+plt.show()
+
+
+tuft_ixs = mdata["milo"].var_names[
+    (mdata["milo"].var["SpatialFDR"] < 0.1)
+    & (mdata["milo"].var["logFC"] > 0)
+    & (mdata["milo"].var["nhood_annotation"] == "Tuft")
+]
+plt.title("Tuft cells")
+milo.plot_nhood_counts_by_cond(
+    mdata, test_var="Hpoly_timecourse", subset_nhoods=tuft_ixs
+)
+plt.show()
+
+# For example, if we take the neighbourhoods of Goblet cells, we can see that
+# neighbourhoods enriched upon infection display a higher expression of Retnlb,
+# which is a gene implicated in anti-parasitic immunity [Haber et al., 2017].
+
+## Compute average Retnlb expression per neighbourhood
+# (you can add mean expression for all genes using milo.utils.add_nhood_expression)
+mdata["rna"].obs["Retnlb_expression"] = (
+    mdata["rna"][:, "Retnlb"].layers["logcounts"].toarray().ravel()
+)
+milo.annotate_nhoods_continuous(mdata, "Retnlb_expression")
+# milo.annotate_nhoods(mdata, "Retnlb_expression")
+
+## Subset to Goblet cell neighbourhoods
+nhood_df = mdata["milo"].var.copy()
+nhood_df = nhood_df[nhood_df["nhood_annotation"] == "Goblet"]
+
+sns.scatterplot(data=nhood_df, x="logFC", y="nhood_Retnlb_expression")
+plt.show()
+
+# In Milo, we can express this type of test design using the syntax ~ 
+# confounder + condition.
+## Make dummy confounder for the sake of this example
+np.random.seed(42)
+nhood_adata = mdata["milo"].copy()
+conf_dict = dict(
+    zip(
+        nhood_adata.obs_names,
+        np.random.choice(["group1", "group2"], nhood_adata.n_obs),
+    )
+)
+mdata["rna"].obs["dummy_confounder"] = [conf_dict[x] for x in mdata["rna"].obs["batch"]]
+
+milo.da_nhoods(mdata, design="~ dummy_confounder+condition")
+mdata["milo"].var
+
---
+++ sc_gse_pathway.py
@@ -0,0 +1,15 @@
+# To determine the pathways enriched in a cell type-specific manner between two
+# conditions, first a relevant collection of gene set signatures is selected, 
+# where each gene set defines a biological process (e.g. epithelial to mesenchymal
+# transition, metabolism etc) or pathway (e.g. MAPK signalling). For each gene set
+# in the collection, DE genes present in the gene set are used to obtain a test 
+# statistic that is then used to assess the enrichment of the gene set. Depending
+# on the type of the enrichment test chosen, gene expression measurements may or 
+# may not be used for the computation of the test statistic.
+
+# Gene sets are a curated list of gene names (or gene ids) that are known to be 
+# involved in a biological process through previous studies and/or experiments. 
+# The Molecular Signatures Database (MSigDB) [Liberzon et al., 2011, Subramanian 
+# et al., 2005] is the most comprehensive database consisting of 9 collections 
+# of gene sets. 
+
